b9b587c6f38df4715afea1ef1e2e7ce44e2c0338
Andrew C. Hopkins <Hopkins.AC@gmail.com>
2021-03-23 14:41:36 -0700

    Tutorial #2 Completed
    Followed the tool to create migrations in the users-service database.  Also fixed the ts-node-dev to actually watch the typescript files as they are modified.

--- services/users-service/src/index.ts
+++ services/users-service/src/index.ts
@@ -1,1 +1,7 @@
-console.log("users-service is up and running");
+import "reflect-metadata"
+
+import { initConnection } from "#root/db/connection"
+
+initConnection().then(() => {
+    console.log("DB connection established!")
+})
