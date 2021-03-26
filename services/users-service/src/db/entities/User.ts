d2aafd48230c44fa4315810c074034450ed4aa3c
Andrew C. Hopkins <Hopkins.AC@gmail.com>
2021-03-26 13:21:17 -0700

    Fixed User.ts Filename Casing
    Fixed the casing from user.ts to User.ts.

--- services/users-service/src/db/entities/User.ts
+++ services/users-service/src/db/entities/User.ts
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ *
+ * Author: Andew C. Hopkins
+ * Timestamp: 2021-03-26T13:16:00 PDT
+ * Copyright (C) 2021
+ * 
+ *******************************************************************************
+ */
+
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("users")
